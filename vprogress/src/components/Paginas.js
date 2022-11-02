c1e44daa236659cf6b255e9f9d1bf06b52b2f07b
unknown <SOPORTE@DESKTOP-FO2M165.enaval.arc>
2022-11-01 22:49:53 -0600

    Generacion paginas de administraciÃn

--- vprogress/src/components/Paginas.js
+++ vprogress/src/components/Paginas.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Inicio } from "./Inicio";
 import { Route, Routes } from "react-router-dom";
 import { ProductosLista } from "./Productos/index";
+import { ResumenVentas } from "./administrador/ResumenVentas";
+import { ResumenProducto } from "./administrador/ResumenProducto";
+import { EditProducto } from "./administrador/EditProducto";
 
 export const Paginas = () => {
   return (
@@ -12,6 +15,9 @@ export const Paginas = () => {
         <Route path="/" element={<Inicio />} />
         {/* Catalogo o Productos de la pagiina */}
         <Route path="/productos/" element={<ProductosLista />} />
+        <Route path="/listventas/" element={<ResumenVentas />} />
+        <Route path="/listproductos/" element={<ResumenProducto />} />
+        <Route path="/editar"  element={<EditProducto />} />
       </Routes>
     </main>
   );
---
+++ vprogress/src/components/administrador/ResumenVentas.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export const ResumenVentas = () => {
+    return (
+        <div><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <div className="bd-example">
+
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Fecha Venta</th>
+                            <th scope="col">Id Venta</th>
+                            <th scope="col">Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>23/02/2022</td>
+                            <td>123</td>
+                            <td>24000</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>24/02/2022</td>
+                            <td>234</td>
+                            <td>32000</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Total</td>
+                            <td>56000</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    );
+};

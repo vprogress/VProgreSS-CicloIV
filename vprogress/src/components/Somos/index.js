16baf9eff3e2e40f3280f955a68b975562335d0b
Manuela Coronado Hernandez <Manuelacoronado20@gmail.com>
2022-11-02 23:01:15 -0500

    SOMOS

--- vprogress/src/components/Somos/index.js
+++ vprogress/src/components/Somos/index.js
@@ -3,99 +3,103 @@ import React from "react";
 export const Somos = () => {
   return (
     <>
-      <h1 className="Tittle">Grupo - VPROGRESS</h1>
-      <div className="Productos"></div>
-      <div>
-        <div className="Display container">
-          <div className="derecha">
-            <div className="presentacion">
-              <div className="img">
-                <img src="./img/Sebastian2.png" alt="Sebastian" />
-              </div>
-              <div className="rol">
-                <div className="descripcion">
-                  <h1>Sebastian Augusto Velasquez Rúgeles</h1>
-                  <h2>Scrum Master</h2>
-                  <p>
-                    Hola soy Sebastian Augusto Velazquez Rúgeles y soy el scrum
-                    master del equipo Vprogress.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div>
+    <div class="container w-100 p-5 banner2 rounded">
+      <div class="row">
+        <aside class="p2">
+          <h2 class="display-6">Equipo De Trabajo</h2>
+          <hr />
+        </aside>
+      </div>
+    </div>
 
-          <div className="izquierda">
-            <div className="presentacion">
-              <div className="img">
-                <img src="./img/Maria2.png" alt="Maria" />
-              </div>
-              <div className="rol">
-                <div className="descripcion">
-                  <h1>Maria Aracelly</h1>
-                  <h2>Development</h2>
-                  <p>
-                    Hola soy Maria Aracelly y soy la desarrolladora del equipo
-                    Vprogress.
-                  </p>
-                </div>
-              </div>
-            </div>
+    <div class="Display container">
+      <div class="derecha">
+        <div class="presentacion pr1">
+          <div class="img">
+            <img src="./img/Sebastian2.png" alt="Sebastian" />
           </div>
-
-          <div className="derecha">
-            <div className="presentacion">
-              <div className="img">
-                <img src="./img/Nelson2.png" alt="Nelson" />
-              </div>
-              <div className="rol">
-                <div className="descripcion">
-                  <h1>Nelson</h1>
-                  <h2>Development</h2>
-                  <p>
-                    Hola soy Nelson y soy el desarrollador del equipo Vprogress.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="izquierda">
-            <div className="presentacion">
-              <div className="img">
-                <img src="./img/Manuela2.png" alt="Manuela" />
-              </div>
-              <div className="rol">
-                <div className="descripcion">
-                  <h1>Manuela Coronado Hernandez</h1>
-                  <h2>Development</h2>
-                  <p>
-                    Hola soy Manuela Coronado Hernandez y soy la desarrolladora
-                    del equipo Vprogress.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="derecha">
-            <div className="presentacion">
-              <div className="img">
-                <img src="./img/Linda2.jpg" alt="Linda" />
-              </div>
-              <div className="rol">
-                <div className="descripcion">
-                  <h1>Linda</h1>
-                  <h2>Development</h2>
-                  <p>
-                    Hola soy Linda y soy la desarrolladora del equipo Vprogress.
-                  </p>
-                </div>
-              </div>
+          <div class="rol">
+            <div class="descripcion">
+              <h1>Sebastian Augusto Velasquez Rúgeles</h1>
+              <h2>Scrum Master</h2>
+              <p>
+                Hola soy Sebastian Augusto Velazquez Rúgeles y soy el scrum master del equipo Vprogress.
+              </p>
             </div>
           </div>
         </div>
       </div>
+  
+      <div class="izquierda">
+        <div class="presentacion pr2">
+          <div class="img">
+            <img src="./img/Maria2.png" alt="Maria" />
+          </div>
+          <div class="rol">
+            <div class="descripcion">
+              <h1>Maria Aracelly</h1>
+              <h2>Development</h2>
+              <p>
+                Hola soy Maria Aracelly y soy la desarrolladora del equipo Vprogress.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="derecha">
+        <div class="presentacion pr3">
+          <div class="img">
+            <img src="./img/Nelson2.png" alt="Nelson" />
+          </div>
+          <div class="rol">
+            <div class="descripcion">
+              <h1>Nelson</h1>
+              <h2>Development</h2>
+              <p>
+                Hola soy Nelson y soy el desarrollador del equipo Vprogress.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="izquierda">
+        <div class="presentacion pr4">
+          <div class="img">
+            <img src="./img/Manuela2.png" alt="Manuela" />
+          </div>
+          <div class="rol">
+            <div class="descripcion">
+              <h1>Manuela Coronado Hernandez</h1>
+              <h2>Development</h2>
+              <p>
+                Hola soy Manuela Coronado Hernandez y soy la desarrolladora del equipo Vprogress.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="derecha">
+        <div class="presentacion pr5">
+          <div class="img">
+            <img src="./img/Linda2.jpg" alt="Linda" />
+          </div>
+          <div class="rol">
+            <div class="descripcion">
+              <h1>Linda</h1>
+              <h2>Development</h2>
+              <p>
+                Hola soy Linda y soy la desarrolladora del equipo Vprogress.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
     </>
   );
 };

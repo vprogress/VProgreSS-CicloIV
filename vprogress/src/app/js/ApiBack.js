a818af49ef47850b9e3bf36ce16ad2a01c087350
nelcambo <nelcambo@gmail.com>
2022-11-19 13:19:42 -0500

    Cambio URL DeleteProduct

--- vprogress/src/app/js/ApiBack.js
+++ vprogress/src/app/js/ApiBack.js
@@ -6,7 +6,7 @@ export const ApiBack = {
      //URL FRONTEND
      INICIO:  "http://127.0.0.1:3000",
      PRODUCT_LIST_ALL: "/api/product/listallproducts",
-     PRODUCT_REMOVE: "/api/product/deleteuser",
+     PRODUCT_REMOVE: "/api/product/deleteproduct",
      PRODUCT_EDIT: "/api/product/updateproduct",
      USER_LOGIN: "/api/public/users/login",
      VENTAS_LIST_ALL: "/api/ventas/listaventas",
